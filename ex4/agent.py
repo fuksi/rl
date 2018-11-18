32163e8ca2d2b84dd47e3de659f890c8824092ec
Phuc Tran <phucdn2005@gmail.com>
2018-11-18 21:09:21 +0200

    Run for task 1

--- ex4/agent.py
+++ ex4/agent.py
@@ -9,12 +9,12 @@ from utils import discount_rewards, softmax_sample
 class Policy(torch.nn.Module):
     def __init__(self, state_space, action_space):
         super().__init__()
+        neurons = 20 
         self.state_space = state_space
         self.action_space = action_space
-
-        # Input state, NN with 20 neurons, Output action
-        self.fc1 = torch.nn.Linear(state_space, 20)
-        self.fc2 = torch.nn.Linear(20, action_space)
+        self.fc1 = torch.nn.Linear(state_space, neurons)
+        self.fc2 = torch.nn.Linear(neurons, action_space)
+        self.fc3 = torch.nn.Linear(neurons, action_space)
         self.init_weights()
 
     def init_weights(self):
@@ -26,8 +26,10 @@ class Policy(torch.nn.Module):
     def forward(self, x):
         x = self.fc1(x)
         x = F.relu(x)
-        x = self.fc2(x)
-        return Normal(x, 1)
+        mu = self.fc2(x)
+        var = self.fc3(x)
+
+        return torch.sigmoid(mu), torch.sigmoid(var)
 
 
 class Agent(object):
@@ -35,7 +37,6 @@ class Agent(object):
         self.train_device = "cpu"  # ""cuda" if torch.cuda.is_available() else "cpu"
         self.policy = policy.to(self.train_device)
         self.optimizer = torch.optim.RMSprop(policy.parameters(), lr=5e-3)
-        self.batch_size = 1
         self.gamma = 0.98
         self.observations = []
         self.actions = []
@@ -53,56 +54,25 @@ class Agent(object):
         loss = torch.sum(weighted_probs)
         loss.backward()
 
-        if (episode_number+1) % self.batch_size == 0:
-            self.update_policy()
-
-    def update_policy(self):
+        # Update policy
         self.optimizer.step()
         self.optimizer.zero_grad()
 
-    def get_action(self, observation, evaluation=False):
+    def get_action(self, observation, episode_number):
         x = torch.from_numpy(observation).float().to(self.train_device)
-        aprob = self.policy.forward(x)
-        if evaluation:
-            pass
-            # action = torch.argmax(aprob).item()
-        else:
-            action = aprob.sample()
-        return action, aprob
+        mu, var = self.policy.forward(x)
 
-    def store_outcome(self, observation, action_output, action_taken, reward):
-        dist = action_output
-        action_taken = torch.Tensor([action_taken]).to(self.train_device)
-        log_action_prob = -dist.log_prob(action_taken)
+        # decay var exponential with decay alpha 0.001
+        var = var * np.exp(-0.0001 * episode_number)
 
-        self.observations.append(observation)
-        self.actions.append(log_action_prob)
+        dist = Normal(torch.tensor(mu), torch.tensor(var))
+        sample = dist.sample()
+        log_prob = dist.log_prob(sample)
+
+        # Limit to [-25, 25] action space
+        action = 50 * sample - 25
+        return action, -log_prob
+
+    def store_outcome(self, reward, action_log_prob):
+        self.actions.append(action_log_prob)
         self.rewards.append(torch.Tensor([reward]))
-
-# class Policy(torch.nn.Module):
-#     def __init__(self, state_space, action_space):
-#         super().__init__()
-#         # Create layers etc
-#         # Initialize neural network weights
-#         self
-#         self.init_weights()
-
-#     def init_weights(self):
-#         for m in self.modules():
-#             if type(m) is torch.nn.Linear:
-#                 torch.nn.init.uniform_(m.weight)
-#                 torch.nn.init.zeros_(m.bias)
-
-#     def forward(self, x):
-#         return 0
-
-
-# class Agent(object):
-#     def __init__(self):
-#         return
-
-#     def episode_finished(self, episode_number):
-#         return
-
-#     def get_action(self, state, evaluation=False):
-#         return np.random.random()*10-5
